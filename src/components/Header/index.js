3e2a8e4046672b457a9d379b9340394dab26d037
Raphael Neves <raphaelneves@MacBook-Air-de-Raphael.local>
2019-07-07 18:20:30 -0300

    Arquivos

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, Title } from "./styles"
+
+export default function Header() {
+    return (
+        <Container>
+            <Title>Nubank</Title>
+        </Container>
+    );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    align-items: center;
+    padding: 40px 0 30px;
+    background: #8B10AE;
+`;
+
+export const Title = styled.Text`
+    font-size: 18px;
+    color: #FFF;
+`;
