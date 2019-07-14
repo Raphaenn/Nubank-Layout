d5e15d2d01a53f5d671ef1db8f21f51248e3221a
Raphael Neves <raphaelneves@MacBook-Air-de-Raphael.local>
2019-07-14 16:50:11 -0300

    card pronto

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Icon from "react-native-vector-icons/MaterialIcons";
 
-import { Container, Code}  from "./styles";
+import { Container, Code, Nav, NavItem, NavText, BotaoSair, TextoSair}  from "./styles";
 
 export default function Menu() {
     return (
@@ -9,6 +9,32 @@ export default function Menu() {
             <Code>
                 <Icon name="view-comfy" size={80} color="#8B10AE"/>
             </Code>
+
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#fff"/>
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#fff"/>
+                    <NavText>Perfil</NavText>
+                </NavItem>
+
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#fff"/>
+                    <NavText>Configuração do cartão ajuda</NavText>
+                </NavItem>
+
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#fff"/>
+                    <NavText>Configuração do app</NavText>
+                </NavItem>
+            </Nav>
+
+            <BotaoSair onPress={() => {}}>
+                <TextoSair>Sair do app</TextoSair>
+            </BotaoSair>
         </Container>
     );
 }
